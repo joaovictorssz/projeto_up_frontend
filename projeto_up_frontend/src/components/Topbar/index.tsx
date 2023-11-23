f91503a07b942669906e27361355f65807c6a243
João Victor Silva <joaovictorsilva.dev@gmail.com>
2023-11-22 21:32:30 -0400

    responsive

--- projeto_up_frontend/src/components/Topbar/index.tsx
+++ projeto_up_frontend/src/components/Topbar/index.tsx
@@ -16,10 +16,11 @@ export default function Topbar(){
         <header className="w-full p-7 flex justify-between border-b border-slate-200">
             <h1 className="invisible"> Ok</h1>
 
-            <span className="sm:block md:hidden lg:hidden">
+            <span className="sm:block flex md:hidden lg:hidden">
                 <Link className="bg-slate-100 p-2 rounded border-2 font-semibold mx-2 text-sm text-sky-800 border-sky-800" href={'/add_family'}>Novo</Link>
                 <Link className="bg-slate-100 p-2 rounded border-2 font-semibold mx-2 text-sm text-sky-800 border-sky-800" href={'/'}>Cadastros</Link>
-                <Link className="bg-slate-100 p-2 rounded border-2 font-semibold mx-2 text-sm text-sky-800 border-sky-800" href={'/users'}>Usuários</Link>
+               <Link className="bg-slate-100 p-2 rounded border-2 font-semibold mx-2 text-sm text-sky-800 border-sky-800" href={'/users'}>Usuários</Link> 
+               <Link className="bg-slate-100 p-2 rounded border-2 font-semibold mx-2 text-sm text-sky-800 border-sky-800" href={'/list_families'}>Meus cadastros</Link>
             </span>
 
             <span className="font-semibold text-sky-800 rounded flex items-center">
